13d3f02120ccd78b2c1368aa1586ac0fa3a32048
Chrische <cs@chrische.net>
2013-02-03 18:35:56 +0100

    Projekt begonnen, Class MensaTag angefangen

--- mensa2irc.rb
+++ mensa2irc.rb
@@ -1,1 +1,12 @@
+class MensaTag
+  attr_accessor :datum, :wochentag, :gerichte
+end
 
+tag = MensaTag.new
+tag.datum = "27.01.2012"
+tag.wochentag = "Mo."
+tag.gerichte = {}
+tag.gerichte["essen1"] = "Steak mit Kartoffeln"
+tag.gerichte["essen2"] = "Erbsensuppe"
+
+tag
