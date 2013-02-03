d47a0fec6a0037d7d29313938240d2adea67486d
Chrische <cs@chrische.net>
2013-02-03 19:00:24 +0100

    Class MensaTag:  initialize + print_tag Methoden

--- mensa2irc.rb
+++ mensa2irc.rb
@@ -1,12 +1,21 @@
 class MensaTag
   attr_accessor :datum, :wochentag, :gerichte
+  def initialize(datum, wochentag)
+    @datum = datum
+    @wochentag = wochentag
+    @gerichte = {} 
+   end
+   def print_tag()
+      print "#{@wochentag} #{@datum}"
+      gerichte.each do |key, value|
+        print " - #{key.upcase}: #{value}"
+      end
+      print "\n"
+   end
 end
 
-tag = MensaTag.new
-tag.datum = "27.01.2012"
-tag.wochentag = "Mo."
-tag.gerichte = {}
+tag = MensaTag.new("27.01.2012", "Mo.")
 tag.gerichte["essen1"] = "Steak mit Kartoffeln"
 tag.gerichte["essen2"] = "Erbsensuppe"
 
-tag
+tag.print_tag
