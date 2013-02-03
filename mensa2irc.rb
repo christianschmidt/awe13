e83c3d3a530427021f245be3acda54c7a68dd08c
Chrische <cs@chrische.net>
2013-02-03 19:28:19 +0100

    MensaTag-Class print_tag -> get_tag

--- mensa2irc.rb
+++ mensa2irc.rb
@@ -7,14 +7,14 @@ class MensaTag
     @datum = datum
     @wochentag = wochentag
     @gerichte = {} 
-   end
-   def print_tag()
-      print "#{@wochentag} #{@datum}"
-      gerichte.each do |key, value|
-        print " - #{key.upcase}: #{value}"
-      end
-      print "\n"
-   end
+  end
+  def get_tag()
+    tag = "#{@wochentag} #{@datum}"
+    @gerichte.each do |key, value|
+       tag << " - #{key.upcase}: #{value}"
+     end
+    return tag
+  end
 end
 
 mensaseite = Nokogiri::HTML(open("http://www.studentenwerk.bremen.de/files/main_info/essen/plaene/uniessen.php"))
@@ -29,7 +29,7 @@ tag = MensaTag.new("25.01.2012", "Fr.")
 tag.gerichte["Essen 1"] = "Steak mit Kartoffeln"
 tag.gerichte["Essen 2"] = "Erbsensuppe"
 
-tag.print_tag
+puts tag.get_tag
 
 5.times do |i|
   datum = mensaseite.xpath("/html/body/table/tr[3]/td[#{i+2}]/font/b").text
@@ -41,6 +41,6 @@ tag.print_tag
   end
 
   woche << tag
-  print tag.print_tag
+  puts tag.get_tag
 end
 
