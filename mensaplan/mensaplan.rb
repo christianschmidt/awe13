7b1440bc01f9ae11c8ae228e439f19861fe832e0
Chrische <cs@chrische.net>
2013-02-07 11:21:45 +0100

    Gemfile und Readme angelegt, Kommentare, Konstanten fuer Einstellungen

--- mensaplan/mensaplan.rb
+++ mensaplan/mensaplan.rb
@@ -18,43 +18,51 @@ class MensaTag
   end
 end
 
+ZIEL_HOST = '192.168.175.13'
+ZIEL_PORT = 6666
+
+# mensaseite per nokogiri holen
 mensaseite = Nokogiri::HTML(open("http://www.studentenwerk.bremen.de/files/main_info/essen/plaene/uniessen.php"))
+# nicht benoetigte zeilenumbrueche duch leerzeichen ersetzen
 mensaseite.search('br').each { |br| br.replace(" ") }
 
+# array fuer wochentage
 woche = []
 
+# positionen der gerichte in der mensaseite angeben (fuer xpath) 
 gerichte_positionen = {}
 gerichte_positionen["Essen 1"] = 4
 gerichte_positionen["Essen 2"] = 6
 gerichte_positionen["Vegetarisch"] = 20
 gerichte_positionen["Wok, Pfanne & Co."] = 28
 
-tag = MensaTag.new("25.01.2012", "Fr.")
-tag.gerichte["Essen 1"] = "Steak mit Kartoffeln"
-tag.gerichte["Essen 2"] = "Erbsensuppe"
 
-puts tag.get_tag
-
+# wochentage durchlaufen
 5.times do |i|
+  # datum und wochentag aus mensaseite holen
   datum = mensaseite.xpath("/html/body/table/tr[3]/td[#{i+2}]/font/b").text
   wochentag = mensaseite.xpath("/html/body/table/tr[2]/td[#{i+2}]/font/b").text
+  
+  # mensaTag Objekt mit datum und wochentag initialisieren
   tag = MensaTag.new(datum, wochentag)
 
+  # alle gewuenschten mahlzeiten durchlaufen und anhand der hinterlegten position aus der mensaseite holen
   gerichte_positionen.each do |key, value|
     tag.gerichte[key] = mensaseite.xpath("/html/body/table/tr[#{value}]/td[#{i+2}]/font/b").text.strip.squeeze(" ")
   end
 
+  # tag in array der woche einfuegen
   woche << tag
+end
+
+# array der woche durchlaufen
+woche.each do |tag|
+  # neuen socket erstellen und zeile des tages senden (workaround hier: immer neuer socket, da zielhost nur einmalig eine zeile annimmt...)
+  s = TCPSocket.new ZIEL_HOST, ZIEL_PORT
+  s.puts tag.get_tag
+  
+  # zeile lokal ausgeben
   puts tag.get_tag
 end
 
-
-woche.each do |tag|
-#  s = TCPSocket.new '192.168.175.13', 6666
-#  s.puts tag.get_tag
-end
-
-s = TCPSocket.new '192.168.175.13', 6666
-s.puts "hi\n"
-s.puts "asd"
 
