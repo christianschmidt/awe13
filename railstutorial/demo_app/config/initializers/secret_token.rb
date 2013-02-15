3f481451a657cc088f4073e15c5078ccab04bcf5
chrische <cs@chrische.net>
2013-02-15 13:59:04 +0100

    erstellt demo_app

---
+++ railstutorial/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '348e5eeff640a34842129498061deca458208988a6aabd10bb1a84af6a3422c708e7fddfb55a3803dba89208e3cced688efe368c204442d93aefff0f33273c64'
